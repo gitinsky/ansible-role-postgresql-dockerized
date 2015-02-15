3adbe96610e194897cd059404db94e7e1c47eec0
Alex Chistyakov <alexclear@gmail.com>
2015-02-15 03:42:37 +0300

    First commit

---
+++ templates/postgresql.rb
@@ -0,0 +1,16 @@
+Eye.application 'postgresql' do
+  working_dir '/'
+  stdall '/var/log/eye/postgresql-stdall.log' # stdout,err logs for processes by default
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
+  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+
+  process :postgresql do
+    pid_file '/var/run/postgresql/9.3-main.pid'
+    start_command 'sudo -u postgres /usr/lib/postgresql/9.3/bin/postmaster -p 5432 --config-file=/etc/postgresql/9.3/main/postgresql.conf'
+
+    start_timeout 10.seconds
+    stop_timeout 5.seconds
+
+  end
+
+end
