3f14bf266080d3f8a814cf9584d615794da44480
Alex Chistyakov <alexclear@gmail.com>
2015-07-03 11:33:37 +0300

    Modify prestart script to set a superuser password

--- templates/prestart.sh
+++ templates/prestart.sh
@@ -6,4 +6,11 @@ chmod 0755 /var/lib/postgresql/9.3
 if [ ! -f /var/lib/postgresql/9.3/main/PG_VERSION ]; then
    /usr/bin/pg_dropcluster 9.3 main
    /usr/bin/pg_createcluster 9.3 main
+   cp /etc/postgresql/9.3/default/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+   cp /etc/postgresql/9.3/default/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+   sudo -u postgres /usr/lib/postgresql/9.3/bin/pg_ctl -w -D /etc/postgresql/9.3/main start
+   sudo -u postgres psql -c "ALTER USER postgres WITH UNENCRYPTED PASSWORD '{{ posgtres_superuser_password }}'"
+   sudo -u postgres /usr/lib/postgresql/9.3/bin/pg_ctl -w -D /etc/postgresql/9.3/main stop
 fi
+cp /etc/postgresql/9.3/default/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+cp /etc/postgresql/9.3/default/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
