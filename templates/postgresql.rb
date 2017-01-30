ddb8fd90b18de9fbece6e68b70481b5e586b3b4e
Alex Chistyakov <alexclear@gmail.com>
2017-01-30 10:33:06 +0300

    Use PostgreSQL 9.6, use trigger

--- templates/postgresql.rb
+++ templates/postgresql.rb
@@ -5,9 +5,13 @@ Eye.application 'postgresql' do
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
 
   process :postgresql do
-    pid_file '/var/run/postgresql/9.3-main.pid'
-    pre_start_command '/root/prestart.sh'
-    start_command 'sudo -u postgres /usr/lib/postgresql/9.3/bin/postmaster -p 5432 --config-file=/etc/postgresql/9.3/main/postgresql.conf'
+    pid_file '/var/run/postgresql/9.6-main.pid'
+    trigger :transition, to: :starting, do: -> {
+      cmd = "/root/prestart.sh"
+      res = process.execute(cmd, process.config.merge(:timeout => 200.seconds))
+      info "executing `#{cmd}` with result: #{res}"
+    }
+    start_command 'sudo -u postgres /usr/lib/postgresql/9.6/bin/postmaster -p 5432 --config-file=/etc/postgresql/9.6/main/postgresql.conf'
 
     start_timeout 10.seconds
     stop_timeout 5.seconds
